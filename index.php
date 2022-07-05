4a91b52d89378b658f2eb2ae131afe9c4ceb7d8e
oskar-anderson <andersonkarloskar@gmail.com>
2022-07-05 23:43:08 +0300

    add github auto pull

--- index.php
+++ index.php
@@ -1,30 +1,42 @@
-<?php
-echo "Hello Heroku!" . '<br>';
+<!doctype html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport"
+         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+</head>
+<body>
+   <h1>Hello Mom!</h1>
+   <?php
 
 
-//Get Heroku ClearDB connection information
+   //Get Heroku ClearDB connection information
 
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$mysqli = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+   $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+   $cleardb_server = $cleardb_url["host"];
+   $cleardb_username = $cleardb_url["user"];
+   $cleardb_password = $cleardb_url["pass"];
+   $cleardb_db = substr($cleardb_url["path"],1);
 
-if ($mysqli -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    exit();
-  }
+   // Connect to DB
+   $mysqli = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-$resource = $mysqli -> query("SELECT * FROM `HelloHeroku`;");
-echo "Returned rows are: " . $resource->num_rows . '<br>';
-while ( $row = $resource->fetch_array() ) {
-   echo "{$row['Name']}"  . '<br>';
-}
-$resource->free();
-$mysqli -> close();
+   if ($mysqli -> connect_errno) {
+      echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+      exit();
+   }
 
-?>
+   $resource = $mysqli -> query("SELECT * FROM `helloheroku`;");
+   echo "Returned rows: {$resource->num_rows} <br>";
+   while ($row = $resource->fetch_array() ) {
+      echo "{$row['Name']}"  . '<br>';
+   }
+   $resource->free();
+   $mysqli -> close();
+
+   ?>
+</body>
+</html>
+
